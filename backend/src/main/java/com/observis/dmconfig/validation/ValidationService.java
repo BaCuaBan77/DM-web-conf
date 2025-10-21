861b19468436a28b3625686e9af2b17ef6f22acc
Long Le <long.le@observis.fi>
2025-10-21 15:32:07 +0300

    fix: correct type for parity

--- backend/src/main/java/com/observis/dmconfig/validation/ValidationService.java
+++ backend/src/main/java/com/observis/dmconfig/validation/ValidationService.java
@@ -27,7 +27,7 @@ public class ValidationService {
     private static final List<String> VALID_SERIAL_PORTS = Arrays.asList("ttyS0", "ttyS1", "/dev/ttyS0", "/dev/ttyS1");
     private static final List<String> VALID_BAUD_RATES = Arrays.asList("9600", "19200", "38400", "57600", "115200");
     private static final List<String> VALID_SERIAL_PORT_TYPES = Arrays.asList("RS232", "RS485");
-    private static final List<String> VALID_PARITY_VALUES = Arrays.asList("None", "Even", "Odd", "N", "E", "O");
+    private static final List<String> VALID_PARITY_VALUES = Arrays.asList("N", "E", "O");
     private static final List<String> VALID_DATA_BITS = Arrays.asList("7", "8");
     private static final List<String> VALID_STOP_BITS = Arrays.asList("1", "2");
 
